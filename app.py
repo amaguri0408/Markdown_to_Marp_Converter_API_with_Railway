9055d1f98d4f8e8cac3819d23dff085ab2090344
Riku Miyata <amaguri0408@gmail.com>
2023-06-16 01:22:09 +0900

    add cors config

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
+import json
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 
 from api.converter import markdown_to_marp, marp_to_markdown
 from api.change_theme import change_theme
 
 app = Flask(__name__)
 
+# config
+with open('config.json') as f:
+    config = json.load(f)
+
+CORS(app, **config['cors'])
+
+
 @app.route('/')
+@cross_origin()
 def home():
     return 'API is running'
 
+
 @app.route('/api/v1/marp', methods=['POST'])
+@cross_origin()
 def convert_to_marp_api():
     # リクエストボディの取得
     data = request.json
@@ -21,7 +33,9 @@ def convert_to_marp_api():
     }
     return jsonify(response), 200
 
+
 @app.route('/api/v1/markdown', methods=['POST'])
+@cross_origin()
 def convert_to_markdown_api():
     # リクエストボディの取得
     data = request.json
@@ -33,7 +47,9 @@ def convert_to_markdown_api():
     }
     return jsonify(response), 200
 
+
 @app.route('/api/v1/theme', methods=['POST'])
+@cross_origin()
 def change_theme_api():
     # リクエストボディの取得
     data = request.json
@@ -45,6 +61,7 @@ def change_theme_api():
         'raw_body': change_theme(raw_body, theme)  # テーマ変更後のMarpデータを設定
     }
     return jsonify(response), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
