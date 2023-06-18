a7479c68df6cae57241d0bca905da46c4e27d5ce
Riku Miyata <amaguri0408@gmail.com>
2023-06-18 23:01:54 +0900

    add [post]/api/v1/upload/image

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import json
+import uuid
+import datetime
+from pathlib import Path
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 
@@ -18,6 +22,35 @@ CORS(app, **config['cors'])
 @cross_origin()
 def home():
     return 'API is running'
+
+
+@app.route('/api/v1/upload/image', methods=['POST'])
+@cross_origin()
+def upload_image_api():
+    # リクエストボディの取得
+    data = request.files
+    image = data['image']
+
+    current_datetime = datetime.datetime.now()
+    formatted_datetime = current_datetime.strftime("%y%m%d_%H%M%S")
+
+    # 画像を保存
+    save_path = Path(config['image_save_path']) / f'{formatted_datetime}_{image.filename}'
+    # すでに存在していたら，数字を足していく
+    i = 1
+    while save_path.exists():
+        save_path = Path(config['image_save_path']) / f'{formatted_datetime}_{i}_{image.filename}'
+        i += 1
+    image.save(save_path)
+
+    # 画像のURLを作成
+    image_url = f"{config['url']}/{config['image_save_path']}/{save_path.name}"
+
+    # レスポンスの作成
+    response = {
+        'url': image_url  # 画像のURLを設定
+    }
+    return jsonify(response), 200
 
 
 @app.route('/api/v1/marp', methods=['POST'])
